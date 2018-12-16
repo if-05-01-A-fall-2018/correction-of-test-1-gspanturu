ee7fc6d2a9f57978ab371ec027fb8fc64ae5bb0c
Gloria Sara Panturu <31956990+gspanturu@users.noreply.github.com>
2018-12-16 22:18:19 +0100

    Correction done

--- parallel.c
+++ parallel.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 //char* arguments[50];
 //char* pointer;
-
+char* arguments[] = { "ls", 0};					     //added to old solution
+char* otherArgs[] = { "ls", "df", "ps", "pwd" };    //added to old solution
 int main(int argc, char const *argv[]) {
-  char* arguments[] = {"ls", 0};            //added to old solution
-  char* otherArgs[] = {"df", "ps", "pwd"};  //added to old solution
-  for (int i = 0; i < 4; i++) {             //added to old solution
-/*  char* input[50];                        //whole block removed from old solution
+	for (int i = 0; i < 4; i++){				   //added to old solution				 
+/*  char* input[50];							  //whole block removed from old solution
   while (1) {
     gets(input);
     char* delim = "&&";
@@ -28,17 +28,14 @@ int main(int argc, char const *argv[]) {
     }
     arguments[count] = 0;
 */
-      int pid = fork();
-      if (pid == 0) {
-        arguments[0] = otherArgs[i];      //added to old solution
-      int err = execvp(arguments[0], arguments);
-        if (err == -1) {
-          printf("Fail\n" );
-        }
-        else{
-          int status;
-          waitpid(-1, &status, 0);
-          }
+      pid_t pid = fork();
+	  if (pid == 0) {
+		  arguments[0] = otherArgs[i];      //added to old solution
+		  execvp(arguments[0], arguments);
+	  }
+	  else {
+		int status;
+        waitpid(-1, &status, 0);
         }
       }/*
     pointer2 = strtok(0, delim2);
